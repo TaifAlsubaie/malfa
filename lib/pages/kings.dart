2c66e4d95b1a60d8a7c5546c7a1a84fb28d0d0cc
Reema Alsubaie <reemar0o08@gmail.com>
2026-02-08 23:38:29 +0300

    Refactor KingsPage and improve layout

--- lib/pages/kings.dart
+++ lib/pages/kings.dart
@@ -182,7 +182,7 @@ class KingsPage extends StatelessWidget {
       margin: const EdgeInsets.only(bottom: 20),
       padding: const EdgeInsets.all(15),
       decoration: BoxDecoration(
-        color: const Color(0xFFC49E61), // تم تعديل اللون هنا
+        color: const Color(0xFFC49E61), 
         borderRadius: BorderRadius.circular(25),
       ),
       child: Row(
@@ -285,3 +285,4 @@ class KingsPage extends StatelessWidget {
     );
   }
 }
+
